547b432f3b23c4f8f540148108ce19a532a558c2
Alice <alicesophiakoch@gmail.com>
2023-04-30 15:16:51 -0300

    Drawing rectangles and changing default colors

--- main/main.py
+++ main/main.py
@@ -5,11 +5,13 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 running = True
-font = pygame.font.Font("../font/pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("../graphics/sky.png").convert_alpha()
 ground_surface = pygame.image.load("../graphics/ground.png").convert_alpha()
-text_surface = font.render("My game", False, "Black")
+
+font = pygame.font.Font("../font/pixeltype.ttf", 50)
+score_surface = font.render("My game", False, (64, 64, 64))
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load("../graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (800, 300))
@@ -24,12 +26,6 @@ while running:
         if event.type == pygame.MOUSEMOTION:
             if player_rect.collidepoint(event.pos):
                 print("Mouse collision with player rect")
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     print(f"Mouse down {event.pos}")
-        # if event.type == pygame.MOUSEBUTTONUP:
-        #     print(f"Mouse up {event.pos}")
-        # if event.type == pygame.MOUSEWHEEL:
-        #     print(f"Mouse wheel {event}")
 
     snail_rect.x -= 4
     if snail_rect.right < 0:
@@ -37,17 +33,12 @@ while running:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    screen.blit(score_surface, score_rect)
 
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
-
-    # if player_rect.colliderect(snail_rect):
-    #     print("snail collision")
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
