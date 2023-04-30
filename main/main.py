d44359be5d5adafd37fd1ae74f0f09ea6c5e3a8b
Alice <alicesophiakoch@gmail.com>
2023-04-29 21:37:54 -0300

    Enemy snail and basic animation

--- main/main.py
+++ main/main.py
@@ -7,18 +7,26 @@ clock = pygame.time.Clock()
 running = True
 font = pygame.font.Font("../font/pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("../graphics/sky.png")
-ground_surface = pygame.image.load("../graphics/ground.png")
+sky_surface = pygame.image.load("../graphics/sky.png").convert_alpha()
+ground_surface = pygame.image.load("../graphics/ground.png").convert_alpha()
 text_surface = font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("../graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 800
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (snail_x_pos, 265))
 
     pygame.display.update()
     clock.tick(60)
